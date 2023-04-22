16b40b5d235484dcf00fdb11e3500c364723fe40
d0lim <limdongyoung0@gmail.com>
2023-04-22 15:13:47 +0900

    feat: add router for CreateAccount handler

--- router/router.go
+++ router/router.go
@@ -10,4 +10,5 @@ func Routes(app *fiber.App) {
 	app.Get("/ping", handler.Pong)
 	app.Post("/google", handler.CallbackJson)
 	app.Post("/v1/login/google", handler.Login)
+	app.Post("/v1/account", handler.CreateAccount)
 }
