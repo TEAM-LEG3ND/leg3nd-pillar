573cf9dabe32066dae7a2256f67ea56d87795c6b
d0lim <limdongyoung0@gmail.com>
2023-04-23 19:48:26 +0900

    refactor: login process to create draft status account and add update account function to handler

--- handler/handler.go
+++ handler/handler.go
@@ -3,48 +3,18 @@ package handler
 import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 	"leg3nd-pillar/auth"
 	"leg3nd-pillar/model"
 	"log"
+	"strconv"
+	"time"
 )
 
 func Auth(ctx *fiber.Ctx) error {
 	path := auth.ConfigGoogle()
 	url := path.AuthCodeURL("state")
 	return ctx.Redirect(url)
-}
-
-func CallbackJson(ctx *fiber.Ctx) error {
-	var req *model.GoogleOAuthUserRequest
-	err := ctx.BodyParser(&req)
-	if err != nil {
-		fmt.Println(err)
-	}
-	token, err := auth.GetGoogleOAuthToken(ctx, req.Code)
-	if err != nil {
-		fmt.Println(err)
-	}
-	user, err := auth.GetGoogleOAuthUser(token)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	ac, err := auth.FindAccountByEmail(user)
-	if err != nil {
-		log.Printf("first error on FindAccountByEmail but do not matter maybe: %v", err)
-		id, err := auth.CreateAccount(user)
-		if err != nil {
-			log.Printf("error occurred when creating account not existed: %v", err)
-			return fmt.Errorf("error occurred when creating account not existed: %v", err)
-		}
-		ac, err = auth.FindAccountById(*id)
-		if err != nil {
-			log.Printf("error occurred when find account by id after creation: %v", err)
-			return fmt.Errorf("error occurred when find account by id after creation: %v", err)
-		}
-	}
-
-	return ctx.Status(200).JSON(ac)
 }
 
 func Login(ctx *fiber.Ctx) error {
@@ -77,35 +47,89 @@ func Login(ctx *fiber.Ctx) error {
 	ac, err := auth.FindAccountByEmail(user)
 	if err != nil {
 		log.Println("Cannot find account by email", err)
+
+		newAccountRequest := &model.NewAccountRequest{
+			Email:         user.Email,
+			FullName:      user.GivenName,
+			OAuthProvider: "google",
+		}
+
+		createdAccountId, err := auth.CreateAccount(newAccountRequest)
+
+		accessToken, err := auth.GetAccessToken(*createdAccountId, time.Minute*1)
+		if err != nil {
+			log.Printf("error occurred on creating access token: %v", err)
+			return fmt.Errorf("error occurred on creating access token: %v", err)
+		}
+
+		tokenResponse := &model.TokenResponse{AccessToken: accessToken}
+
 		return ctx.Status(fiber.StatusUnauthorized).JSON(model.FindAccountByEmailFailedResponse{
-			Code:             model.NewUser,
-			Message:          "Cannot find account by email",
-			OAuthAccessToken: token.AccessToken,
-			OAuthProvider:    "google",
+			Code:    model.NewUser,
+			Message: "Cannot find account by email",
+			Token:   *tokenResponse,
 		})
 	}
 
-	return ctx.Status(200).JSON(ac)
+	accessToken, err := auth.GetAccessToken(ac.Id, time.Minute*30)
+	if err != nil {
+		log.Printf("error occurred on creating access token: %v", err)
+		return fmt.Errorf("error occurred on creating access token: %v", err)
+	}
+	refreshToken, err := auth.GetRefreshToken(ac.Id, time.Hour*24*30)
+	if err != nil {
+		log.Printf("error occurred on creating refresh token: %v", err)
+		return fmt.Errorf("error occurred on creating refresh token: %v", err)
+	}
+
+	ctx.Cookie(&fiber.Cookie{
+		Name:  "refresh_token",
+		Value: *refreshToken,
+		//Path:        "",
+		//Domain:      "",
+		MaxAge:      60 * 60 * 24 * 30,
+		Expires:     time.Now().Add(time.Hour * 24 * 30),
+		Secure:      true,
+		HTTPOnly:    true,
+		SameSite:    "none",
+		SessionOnly: false,
+	})
+
+	tokenResponse := &model.TokenResponse{
+		AccessToken: accessToken,
+	}
+
+	return ctx.Status(200).JSON(tokenResponse)
 }
 
-func CreateAccount(ctx *fiber.Ctx) error {
-	var req *model.NewAccountRequest
-	err := ctx.BodyParser(&req)
+func UpdateAccount(ctx *fiber.Ctx) error {
+	draftUser := ctx.Locals("user").(*jwt.Token)
+	claims := draftUser.Claims.(jwt.MapClaims)
+	sub := claims["sub"].(string)
+	id, err := strconv.ParseInt(sub, 10, 64)
 	if err != nil {
-		return &fiber.Error{Code: fiber.StatusBadRequest, Message: "Invalid Request. Check your request body"}
+		log.Printf("error occurred when parsing jwt sub into int64, %v", err)
+		return fmt.Errorf("error occurred when parsing jwt sub into int64, %w", err)
 	}
-	id, err := auth.CreateAccountV1(req)
-	if err != nil {
-		log.Printf("error occurred when creating account not existed: %v", err)
-		return fmt.Errorf("error occurred when creating account not existed: %v", err)
-	}
-	ac, err := auth.FindAccountById(*id)
+
+	_, err = auth.FindAccountById(id)
 	if err != nil {
 		log.Printf("error occurred when find account by id after creation: %v", err)
 		return fmt.Errorf("error occurred when find account by id after creation: %v", err)
 	}
 
-	return ctx.Status(fiber.StatusCreated).JSON(ac)
+	var updateAccountRequestBody *model.UpdateAccountRequestBody
+	if err := ctx.BodyParser(&updateAccountRequestBody); err != nil {
+		return ctx.Status(400).JSON(fiber.Map{"message": "Invalid update account body"})
+	}
+
+	_, err = auth.UpdateAccount(id, updateAccountRequestBody)
+	if err != nil {
+		log.Printf("error occurred while updating account by id: %v", err)
+		return fmt.Errorf("error occurred while updating account by id: %v", err)
+	}
+
+	return ctx.SendStatus(fiber.StatusOK)
 }
 
 func Pong(ctx *fiber.Ctx) error {
