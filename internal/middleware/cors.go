ade94081470483d975cba69c50f5442d2db4c80a
d0lim <limdongyoung0@gmail.com>
2023-05-05 19:15:25 +0900

    fix: sample cors settings for authorization

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -7,8 +7,8 @@ import (
 
 func Cors() fiber.Handler {
 	return cors.New(cors.Config{
-		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
-		AllowOrigins:     "*",
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin,Authorization",
+		AllowOrigins:     "https://google-login-demo.vercel.app",
 		AllowCredentials: true,
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	})
