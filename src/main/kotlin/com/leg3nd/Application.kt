1c29de6ed82efb0033e36064ed2235c952607ec5
d0lim <limdongyoung0@gmail.com>
2023-07-03 22:50:15 +0900

    feat: apply ContentNegotiation module

--- src/main/kotlin/com/leg3nd/Application.kt
+++ src/main/kotlin/com/leg3nd/Application.kt
@@ -1,9 +1,13 @@
 package com.leg3nd
 
+import com.leg3nd.plugins.configureHTTP
+import com.leg3nd.plugins.configureRouting
+import com.leg3nd.plugins.configureSecurity
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.leg3nd.plugins.*
+import io.ktor.server.plugins.contentnegotiation.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -11,6 +15,9 @@ fun main() {
 }
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
     configureHTTP()
     configureSecurity()
     configureRouting()
