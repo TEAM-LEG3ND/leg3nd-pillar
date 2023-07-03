4e30403e4fec2e47f9b83fc5ea0e91ed52f38c18
d0lim <limdongyoung0@gmail.com>
2023-07-03 22:50:22 +0900

    chore: write simple sample

--- src/main/kotlin/com/leg3nd/plugins/Routing.kt
+++ src/main/kotlin/com/leg3nd/plugins/Routing.kt
@@ -1,10 +1,11 @@
 package com.leg3nd.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
-import io.ktor.server.plugins.statuspages.*
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
     install(StatusPages) {
@@ -14,7 +15,17 @@ fun Application.configureRouting() {
     }
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            val lb = listOf(Foo.Bar("hi"), Foo.Bar("my name is"), Foo.Bar("d0lim"))
+            val f = Foo(lb)
+            call.respond(f)
         }
     }
+}
+
+@Serializable
+data class Foo(
+    val some: List<Bar>
+) {
+    @Serializable
+    data class Bar(val name: String)
 }
