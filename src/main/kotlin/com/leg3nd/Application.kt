34fad7c018ba79eed428b5c10f5595de7c4eb7be
D0Lim <limdongyoung0@gmail.com>
2023-07-15 16:58:53 +0700

    feat: Koin 적용 (#28)
    * feat: add koin and koin ktor as dependency
    
    * feat: initialize koin
    
    * feat: install ktlint
    
    * chore: upgrade kotlin version to 1.9.0
    
    * feat: enable ksp for koin annotation
    
    * feat: apply ksp plugin and modify koin core to implementation
    
    * feat: add Cat domain model
    
    * feat: add Database and Service Port
    
    * feat: add CatEntity
    
    * feat: add simple CatRepository
    
    * feat: implement CatDatabaseAdapter
    
    * feat: implement CatServiceImpl
    
    * feat: add CatController
    
    * feat: add simple KoinModule Component scan config
    
    * feat: inject CatController to Routing
    
    * feat: initialize KoinModule in Application.kt

--- src/main/kotlin/com/leg3nd/Application.kt
+++ src/main/kotlin/com/leg3nd/Application.kt
@@ -1,5 +1,6 @@
 package com.leg3nd
 
+import com.leg3nd.plugins.KoinModule
 import com.leg3nd.plugins.configureHTTP
 import com.leg3nd.plugins.configureRouting
 import com.leg3nd.plugins.configureSecurity
@@ -8,6 +9,10 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
+import org.koin.dsl.module
+import org.koin.ksp.generated.module
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -17,6 +22,10 @@ fun main() {
 fun Application.module() {
     install(ContentNegotiation) {
         json()
+    }
+    install(Koin) {
+        slf4jLogger()
+        modules(KoinModule().module)
     }
     configureHTTP()
     configureSecurity()
--- src/main/kotlin/com/leg3nd/plugins/Routing.kt
+++ src/main/kotlin/com/leg3nd/plugins/Routing.kt
@@ -1,31 +1,24 @@
 package com.leg3nd.plugins
 
-import io.ktor.http.*
+import com.leg3nd.application.CatController
 import io.ktor.server.application.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.Serializable
+import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
     install(StatusPages) {
         exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+            call.respondText(text = "500: $cause", status = io.ktor.http.HttpStatusCode.InternalServerError)
         }
     }
-    routing {
-        get("/") {
-            val lb = listOf(Foo.Bar("hi"), Foo.Bar("my name is"), Foo.Bar("d0lim"))
-            val f = Foo(lb)
-            call.respond(f)
-        }
-    }
-}
 
-@Serializable
-data class Foo(
-    val some: List<Bar>
-) {
-    @Serializable
-    data class Bar(val name: String)
+    val catController by inject<CatController>()
+
+    routing {
+        get("/cats/name") {
+            call.respond(catController.getAllCatNames())
+        }
+    }
 }
