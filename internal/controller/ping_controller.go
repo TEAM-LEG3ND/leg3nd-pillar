869232e74300a6136a4eb1d7870ae7ab0a82f5d4
d0lim <limdongyoung0@gmail.com>
2023-08-27 17:13:34 +0900

    refactor: add xAccountId for ping

--- internal/controller/ping_controller.go
+++ internal/controller/ping_controller.go
@@ -3,5 +3,6 @@ package controller
 import "github.com/gofiber/fiber/v2"
 
 func Pong(ctx *fiber.Ctx) error {
-	return ctx.Status(fiber.StatusOK).SendString("pong")
+	xAccountId := ctx.GetReqHeaders()["x-account-id"]
+	return ctx.Status(fiber.StatusOK).SendString("pong, " + xAccountId)
 }
